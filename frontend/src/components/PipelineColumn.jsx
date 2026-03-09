62a54e3f54da281f857984e49caaea5b390c7aa3
Areaxis <areaxismygod@gmail.com>
2026-03-09 18:43:12 +0530

    Added reusable pipeline column component for lead stages

--- frontend/src/components/PipelineColumn.jsx
+++ frontend/src/components/PipelineColumn.jsx
@@ -1,18 +1,36 @@
 import { memo } from "react";
 
-function PipelineColumn({ title, leads }) {
+function PipelineColumn({title,leads}){
 
   return (
-    <div>
 
-      <h3>{title}</h3>
+    <div style={{
+      minWidth:"200px",
+      border:"1px solid #ddd",
+      padding:"10px"
+    }}>
 
-      {leads.map((lead) => (
-        <div key={lead._id}>{lead.name}</div>
+      <h4>{title}</h4>
+
+      {leads.map(lead => (
+
+        <div key={lead._id} style={{
+          border:"1px solid #ccc",
+          padding:"5px",
+          marginBottom:"5px"
+        }}>
+
+          <strong>{lead.name}</strong>
+          <p>{lead.phone}</p>
+
+        </div>
+
       ))}
 
     </div>
+
   );
+
 }
 
 export default memo(PipelineColumn);
