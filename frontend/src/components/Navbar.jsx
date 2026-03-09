3a354c5dc58e3719f95344a936855a695532bcb9
Areaxis <areaxismygod@gmail.com>
2026-03-09 20:16:23 +0530

    Fixed HTML structuring

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,15 +4,12 @@ export default function Navbar() {
   return (
     <div>
 
-      <nav style={{ display: "flex", gap: "20px" }}>
+      <nav className="flex gap-4 p-4 bg-gray-200">
         <Link to="/">Dashboard</Link>
         <Link to="/leads">Leads</Link>
         <Link to="/visits">Visits</Link>
       </nav>
 
-      <hr />
-
-      {/* child routes render here */}
       <Outlet />
 
     </div>
