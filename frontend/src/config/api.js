120d9c5b1f4897b86197ebfc630e001e63584019
Areaxis <areaxismygod@gmail.com>
2026-03-11 14:38:27 +0530

    Changed baseURL of axios api to vercel app

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://gharpayy-crm-mvp-api.vercel.app",
   timeout: 5000
 });
 
